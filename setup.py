77d71f8d9cb642b736f18fc57e73baca11c8008f
Fahad Khan <fahadkhan6302@gmail.com>
2020-06-02 17:26:23 +0500

    update package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PyGoogleDrive',
-    version='1.0.0',
+    version='1.0.1',
     description='Google Drive API made easy with PyGoogleDrive',
     long_description=open('README.rst').read(),
     packages=['PyGoogleDrive'],
